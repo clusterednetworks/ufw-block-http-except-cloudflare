9fa84a28e04d2f8aa7186fedba85e5b01062d2d7
clusterednetworks <68355792+clusterednetworks@users.noreply.github.com>
2020-07-15 16:43:10 -0600

    Add files via upload

---
+++ cloudflare-ufw.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This file had been modified by clusterednetworks.com
+# 2018-11-20
+DIR="$(dirname $(readlink -f $0))"
+cd $DIR
+wget https://www.cloudflare.com/ips-v4 -O ips-v4.tmp
+wget https://www.cloudflare.com/ips-v6 -O ips-v6.tmp
+mv ips-v4.tmp ips-v4
+mv ips-v6.tmp ips-v6
+
+for cfip in `cat ips-v4`; do ufw allow proto tcp from $cfip to any port 80,443; done
+for cfip in `cat ips-v6`; do ufw allow proto tcp from $cfip to any port 80,443; done
+
+ufw reload > /dev/null
+
+# OTHER EXAMPLE RULES
+# Examples to restrict to port 80
+#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
+#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
+
+# Examples to restrict to port 443
+#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
+#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
