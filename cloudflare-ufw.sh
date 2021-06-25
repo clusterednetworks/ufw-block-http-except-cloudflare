2381a4044f9628f9e2bc531062782b7339178c9c
Clustered Networks <68355792+clusterednetworks@users.noreply.github.com>
2021-06-25 15:01:18 -0600

    Update cloudflare-ufw.sh

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 # This file had been modified by clusterednetworks.com
 # 2018-11-20
-# Copyright (C) 2018  <ron@clusterednetworks.com>
-
+# Copyright (C) 2018  <info@clusterednetworks.com>
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 2 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+# 
 # This file is free software; as a special exception the author gives
 # unlimited permission to copy and/or distribute it, with or without
 # modifications, as long as this notice is preserved.
